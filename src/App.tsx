3a7a141a5b67a0be123e8b0889fb4afc49164f12
mohamed-tarek-fouad <mt1348@fayoum.edu.eg>
2023-05-31 19:03:58 +0300

    plans and reasons

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import './App.css';
 import Hero from './components/Hero/hero';
 import Programms from './components/programms/programms';
+import Reasons from './components/reasons/reasons';
+import Plans from './components/plans/plans';
 
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programms />
+      <Reasons />
+      <Plans />
     </div>
   );
 }
